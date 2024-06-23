70cdb8ef744439d85d6113432b48bb39ecd891b4
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-23 07:15:54 -0400

    Se creó un decorardor personalizado para obtener el usuario del JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +21,14 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+  ) {
     return {
       ok: true,
       message: 'Hola Mundo Private',
+      user
     };
   }
 }
---
+++ src/auth/decorators/index.ts
@@ -0,0 +1,3 @@
+export { GetUser } from "./get-user.decorator";
+
+
