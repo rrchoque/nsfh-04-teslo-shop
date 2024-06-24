e7434601eee71b99d736ff12dc00b95430300ce5
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-24 17:00:01 -0400

    Se agregaron usuarios de prueba en el seed, y en los productos

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -1,3 +1,5 @@
+import * as bcrypt from 'bcrypt';
+
 interface SeedProduct {
     description: string;
     images: string[];
@@ -14,13 +16,40 @@ interface SeedProduct {
 type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
 type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
 
+interface SeedUser {
+    email:    string;
+    fullName: string;
+    password: string;
+    roles:     string[];
+}
 
 interface SeedData {
+    users: SeedUser[];
     products: SeedProduct[];
 }
 
 
 export const initialData: SeedData = {
+    users: [
+        {
+            email: 'reynaldo@google.com',
+            fullName: 'Reynaldo Choque',
+            password: bcrypt.hashSync( '123.abcD', 10 ),
+            roles: ['user', 'super-admin']
+        },
+        {
+            email: 'test1@google.com',
+            fullName: 'Test One',
+            password: bcrypt.hashSync( 'Abc123', 10 ),
+            roles: ['user']
+        },
+        {
+            email: 'test2@google.com',
+            fullName: 'Test Two',
+            password: bcrypt.hashSync( 'Abc123', 10 ),
+            roles: ['user','admin']
+        }
+    ],
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,27 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
 import { initialData } from './data/seed-data';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly productsService: ProductsService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   async executeSeed() {
-    await this.insertNewProducts();
-
+    await this.deleteTables();
+    const firtUser = await this.insertUsers();
+    await this.insertNewProducts(firtUser);
     return 'SEED EXECUTED';
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
     await this.productsService.deleteAllProducts();
 
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder.delete().where({}).execute();
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+
+    seedUsers.forEach((user) => {
+      users.push(this.userRepository.create(user));
+    });
+
+    const dbUsers = await this.userRepository.save(seedUsers);
+
+    return dbUsers[0];
+  }
+
+  private async insertNewProducts(user: User) {
     const products = initialData.products;
 
     const insertPromises = [];
 
-    // products.forEach((product) => {
-    //   insertPromises.push(this.productsService.create(product));
-    // });
+    products.forEach((product) => {
+      insertPromises.push(this.productsService.create(product, user));
+    });
 
     await Promise.all(insertPromises);
 
