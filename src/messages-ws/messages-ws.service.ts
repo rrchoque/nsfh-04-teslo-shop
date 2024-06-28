5c9bb15fe1a2afb6cfac598395d5a15afec43f76
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-28 00:01:15 -0400

    Se creó un registro de las conexiones

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
+interface ConnectedClients {
+  [id: string]: {
+    socket: Socket;
+  };
+}
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+  private connectedClients: ConnectedClients = {};
+
+  async registerClient(client: Socket) {
+    this.connectedClients[client.id] = {
+      socket: client,
+    };
+  }
+
+  removeClient(clientId: string) {
+    delete this.connectedClients[clientId];
+  }
+
+  getConnectedClients(): number {
+    return Object.keys(this.connectedClients).length;
+  }
+}
